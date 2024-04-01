1b9fe5fcf5563bcd7aec638946dcf18bd054460c
Iván Yamil Romero <ivanromeroprog@gmail.com>
2024-03-31 23:13:48 -0300

    control de errores en endpoint api login

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -11,13 +11,13 @@ async function handler(req: NextRequest) {
 
   const response = await login({ email, password });
 
-  const accessToken = response.data?.data?.tokens?.access_token;
-  const refreshToken = response.data?.data?.tokens?.refresh_token;
-  const userData = response.data?.data?.user;
+  const accessToken = response?.data?.data?.tokens?.access_token;
+  const refreshToken = response?.data?.data?.tokens?.refresh_token;
+  const userData = response?.data?.data?.user;
 
   //Si no vino el access token devolver status y respuesta del server
   if (!accessToken) {
-    return NextResponse.json(response?.data, { status: response.status });
+    return NextResponse.json(response?.data ?? [], { status: response?.status });
   }
 
   //Guardar access token para usar en server
